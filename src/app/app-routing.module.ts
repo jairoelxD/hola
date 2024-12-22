69698783961401a654f8c1d1702e97ffd52e8a21
unknown <lasleyendas1@gmail.com>
2024-12-22 19:55:42 -0400

    actividad6

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AcercadeComponent } from './pages/acercade/acercade.component';  
+import { InicioComponent } from './pages/inicio/inicio.component';
+
+const routes: Routes = [
+  {path: 'inicio', component: InicioComponent},
+  {path: 'acercade', component: AcercadeComponent},
+  {path: '**', component: InicioComponent},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
+  exports: [RouterModule]
+
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { InicioComponent } from './pages/inicio/inicio.component';
+import { AcercadeComponent } from './pages/acercade/acercade.component';
+import { NavbarComponent } from './component/navbar/navbar.component';
+import { FooterComponent } from './component/footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    InicioComponent,
+    AcercadeComponent,
+    NavbarComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
